9e1edc96658e3a8df806eda6a7d80a2f9fada73a
JuanmarDiez <juanmartindiez59@gmail.com>
2025-05-23 11:30:17 -0300

    Base proyecto spring

---
+++ TP_OO2_Turnos/src/main/java/com/example/TP_OO2_Turnos/TpOo2TurnosApplication.java
@@ -0,0 +1,13 @@
+package com.example.TP_OO2_Turnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpOo2TurnosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpOo2TurnosApplication.class, args);
+	}
+
+}
---
+++ TP_OO2_Turnos/src/test/java/com/example/TP_OO2_Turnos/TpOo2TurnosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TP_OO2_Turnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpOo2TurnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
