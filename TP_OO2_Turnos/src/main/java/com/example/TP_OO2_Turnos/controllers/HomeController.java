21959b083b543e2b62384e08471b46dfd59063f7
ArielGonzaloMendoza <gonzalomendozaariel@gmail.com>
2025-06-04 20:57:14 -0300

    Implemente la vista html de inicio de sesion y registro

---
+++ TP_OO2_Turnos/src/main/java/com/example/TP_OO2_Turnos/controllers/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.TP_OO2_Turnos.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home"; // Vista con las dos opciones
+    }
+    
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "login";
+    }
+    
+    @GetMapping("/register")
+    public String showRegisterForm() {
+        return "register";
+    }
+}
