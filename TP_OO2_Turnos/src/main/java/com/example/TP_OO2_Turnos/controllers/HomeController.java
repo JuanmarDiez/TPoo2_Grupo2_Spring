1b99262a3e773d13bb90c1211f3c8d5e3bcc035a
ArielGonzaloMendoza <gonzalomendozaariel@gmail.com>
2025-06-14 21:07:15 -0300

    Mejore las vistas login e index y separe las opciones generar y consultar turnos segun el rol

--- TP_OO2_Turnos/src/main/java/com/example/TP_OO2_Turnos/controllers/HomeController.java
+++ TP_OO2_Turnos/src/main/java/com/example/TP_OO2_Turnos/controllers/HomeController.java
@@ -1,18 +1,10 @@
 package com.example.TP_OO2_Turnos.controllers;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.view.RedirectView;
 
-import com.example.TP_OO2_Turnos.entities.Usuario;
-import com.example.TP_OO2_Turnos.exception.UnauthorizedException;
 import com.example.TP_OO2_Turnos.services.IDiaService;
 import com.example.TP_OO2_Turnos.services.IUsuarioService;
 
